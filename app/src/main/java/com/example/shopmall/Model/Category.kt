241a574726c8bdec36264796b9981f57260ab578
Javokhir3055 <rasulovjavohir1@gmail.com>
2020-05-04 11:09:56 +0500

    lesson 02

--- app/src/main/java/com/example/shopmall/Model/Category.kt
+++ app/src/main/java/com/example/shopmall/Model/Category.kt
@@ -1,4 +1,8 @@
 package com.example.shopmall.Controller
 
 
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
