a529c6996d521e9bca760623bf6bc21be4aa1e96
Javokhir3055 <rasulovjavohir1@gmail.com>
2020-05-04 12:27:43 +0500

    lesson-03

--- app/src/main/java/com/example/shopmall/Controller/MainActivity.kt
+++ app/src/main/java/com/example/shopmall/Controller/MainActivity.kt
@@ -3,21 +3,20 @@ package com.example.shopmall.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.shopmall.Adapters.CategoryAdapter
 import com.example.shopmall.R
 import com.example.shopmall.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_activated_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
